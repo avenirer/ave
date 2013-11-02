c12bf1dce780286420328a9dab3df0b758de89f9
Adrian Voicu <avenir.ro@gmail.com>
2013-11-03 00:15:53 +0200

    changed default title to Ave

--- application/views/head_view.php
+++ application/views/head_view.php
@@ -1,10 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <!DOCTYPE html>
-<html lang="ro">
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>
-        <?php echo isset($title) ? $title.' - AvenirERP' :  'AvenirERP'; ?>
+        <?php echo isset($title) ? $title.' - Ave' :  'Ave'; ?>
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
